46e6389aab8d925c45440b890cfa6d7fad3a7a9b
Wu Wenhao <1321471639@qq.com>
2023-12-05 11:03:38 +0800

    update

--- router/router.go
+++ router/router.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 
@@ -15,8 +17,21 @@ func InitRouter(e *gin.Engine) {
 	e.LoadHTMLGlob("templates/*.html")
 	e.Static("/assets", "./assets")
 
-	e.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{"title": "一个简单的文件服务应用 - 首页"})
+	e.GET("/", func(c *gin.Context) {
+		files, err := filepath.Glob(filepath.Join(config.FileStorage, "*"))
+		if err != nil {
+			panic(err)
+		}
+
+		var links []string
+		for _, file := range files {
+			links = append(links, strings.TrimPrefix(file, config.FileStorage+"/"))
+		}
+
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "一个简单的文件服务应用 - 首页",
+			"files": links,
+		})
 	})
 
 	e.POST("/upload", func(c *gin.Context) {
@@ -48,6 +63,58 @@ func InitRouter(e *gin.Engine) {
 				"path":     config.FileStorage,
 				"filename": handler.Filename,
 			},
+		})
+	})
+
+	e.GET("/download", func(c *gin.Context) {
+		filename := c.Query("filename")
+		if filename == "" {
+			c.String(http.StatusOK, "无下载资源~")
+			return
+		}
+
+		file, err := os.Open(filename)
+		if err != nil {
+			_ = c.AbortWithError(http.StatusNotFound, err)
+			return
+		}
+		defer file.Close()
+
+		fi, err := file.Stat()
+		if err != nil {
+			_ = c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		c.Writer.WriteHeader(http.StatusOK)
+		c.Header("Content-Disposition", "attachment; filename="+fi.Name())
+		c.Header("Content-Type", "application/octet-stream")
+		_, _ = io.Copy(c.Writer, file)
+	})
+
+	e.POST("/delete", func(c *gin.Context) {
+		filename := c.PostForm("filename")
+
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    http.StatusNotFound,
+				"message": "删除资源不存在",
+			})
+			return
+		}
+
+		err := os.Remove(filename)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code":    -1,
+				"message": "删除失败",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"code":    0,
+			"message": "删除成功",
 		})
 	})
 
